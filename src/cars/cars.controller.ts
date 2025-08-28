d670a726cf9098c8fa1986d0f4358394f1baf899
yunioseijo <yunioseijo@gmail.com>
2025-08-28 13:51:22 +0200

    actualización del controlador con los métodos del CRUD pero sin implementar

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -9,7 +9,24 @@ export class CarsController {
     return this.carsService.findAll();
   }
   @Get(':id')
-  getCarById(@Param('id') id: string) {
-    return this.carsService.findOneById(Number(id));
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
+  }
+  @Post()
+  createCar(@Body() body: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return body;
+  }
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return body;
+  }
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id: id,
+    };
   }
 }
